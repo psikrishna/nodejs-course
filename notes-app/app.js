4d838ce215778e3679ce7a14a8f0efea2c9f246c
psikrishna <attalurisaikrishna@gmail.com>
2020-06-20 03:25:24 +0530

    moduminor changes #0

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,4 @@
 const log =console.log
-const greenText=require('chalk')
-log(greenText.inverse.bold.green(' dodo '));
+const formatOutput=require('chalk')
+const getNotes=require('./notes')
+log(formatOutput.inverse.bold.green(' dodo '));
