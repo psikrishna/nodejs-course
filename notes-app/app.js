292cd8bcd8f97a748923c438e72d1136db56b8a4
psikrishna <attalurisaikrishna@gmail.com>
2020-06-20 14:26:58 +0530

    user input cli tutorial

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,16 @@
-const log =console.log
-const formatOutput=require('chalk')
-const getNotes=require('./notes')
-log(formatOutput.inverse.bold.green(' dodo '));
+const log =console.log;
+const formatOutput=require('chalk');
+const getNotes=require('./notes');
+const command=process.argv[2];
+// log(formatOutput.inverse.bold.green(' dodo '));
+// log(process.argv[2]);
+if(command==='add'){
+    log('Adding Note');
+}
+else if(command==='remove'){
+    log('Removing Note');
+}
+else{
+    log('ripe dodo');
+}
+log(process.argv)
