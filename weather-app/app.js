ec2a85488effd38c7296c8b57a7776fe529c5cab
psikrishna <attalurisaikrishna@gmail.com>
2020-06-21 21:26:48 +0530

    minor changes #7

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,11 +2,8 @@ const apiConfig = require('../apiKeys')
 const request = require('postman-request');
 const log = console.log;
 
-//log(apiConfig.weather_api_key);
-
 const url = 'http://api.weatherstack.com/current?access_key=' + apiConfig.weather_api_key + '&query=New%20York';
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body);
-    log(data);
+request({ url: url, json: true }, (error, response) => {
+    log(response.body.current);
 });
