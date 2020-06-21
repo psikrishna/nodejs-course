0820cab989dc0ea8c448b1be67696dac40eb1c24
psikrishna <attalurisaikrishna@gmail.com>
2020-06-22 01:23:47 +0530

    geocode refactoring done

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,36 +1,25 @@
 const apiConfig = require('../apiKeys');
 const request = require('postman-request');
 const chalk = require('chalk');
+const geocode = require('./utils/geocode')
 const log = console.log;
 
-const url = 'http://api.weatherstack.com/current?access_key=' + apiConfig.weather_api_key + '&query=Lucknow&units=m';
-const geoUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Lucknow.json?access_token=' + apiConfig.mapbox_api_key + '&limit=1';
-
-const location = 'Lucknow';
-
-request({ url: geoUrl, json: true }, (error, response) => {
-    if (error) {
-        log(chalk.red.inverse('error in connection'));
-    }
-    else if (response.body.features.length === 0) {
-        log(chalk.red.inverse('unable to find location'));
-    }
-    else {
-        const lat = response.body.features[0].center[1];
-        const lng = response.body.features[0].center[0];
-        log(lat, lng);
-    }
+geocode('Allahabad', (error, data) => {
+    log('error:', error)
+    log('data:', data)
 });
 
-request({ url: url, json: true }, (error, response) => {
-    if (error) {
-        log(chalk.red.inverse('error in connection'));
-    }
-    else if (response.body.error) {
-        log(chalk.red.inverse('unable to find location'));
-    }
-    else {
-        log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degrees out. There is a ' + response.body.current.precip + ' % chance of rain.');
-    }
+//const url = 'http://api.weatherstack.com/current?access_key=' + apiConfig.weather_api_key + '&query=' + lat + ',' + lng + '&units=m';
 
-});
+// request({ url: url, json: true }, (error, response) => {
+//     if (error) {
+//         log(chalk.red.inverse('error in connection'));
+//     }
+//     else if (response.body.error) {
+//         log(chalk.red.inverse('unable to find location'));
+//     }
+//     else {
+//         log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degrees out. There is a ' + response.body.current.precip + ' % chance of rain.');
+//     }
+
+// });
