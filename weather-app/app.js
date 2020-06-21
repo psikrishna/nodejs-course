00707943704fc045a9a1884e1f62a50083cd59d0
psikrishna <attalurisaikrishna@gmail.com>
2020-06-22 00:29:40 +0530

    minor changes #11

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,17 +1,36 @@
 const apiConfig = require('../apiKeys');
 const request = require('postman-request');
+const chalk = require('chalk');
 const log = console.log;
 
 const url = 'http://api.weatherstack.com/current?access_key=' + apiConfig.weather_api_key + '&query=Lucknow&units=m';
 const geoUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Lucknow.json?access_token=' + apiConfig.mapbox_api_key + '&limit=1';
 
-// request({ url: url, json: true }, (error, response) => {
-//     //log(response.body.current);
-//     log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degrees out. There is a ' + response.body.current.precip + ' % chance of rain.')
-// });
+const location = 'Lucknow';
 
 request({ url: geoUrl, json: true }, (error, response) => {
-    const lat = response.body.features[0].center[1];
-    const lng = response.body.features[0].center[0];
-    log(lat, lng);
+    if (error) {
+        log(chalk.red.inverse('error in connection'));
+    }
+    else if (response.body.features.length === 0) {
+        log(chalk.red.inverse('unable to find location'));
+    }
+    else {
+        const lat = response.body.features[0].center[1];
+        const lng = response.body.features[0].center[0];
+        log(lat, lng);
+    }
+});
+
+request({ url: url, json: true }, (error, response) => {
+    if (error) {
+        log(chalk.red.inverse('error in connection'));
+    }
+    else if (response.body.error) {
+        log(chalk.red.inverse('unable to find location'));
+    }
+    else {
+        log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degrees out. There is a ' + response.body.current.precip + ' % chance of rain.');
+    }
+
 });
