32ea033d04502f5f2378c00491711756609f1824
psikrishna <attalurisaikrishna@gmail.com>
2020-06-22 01:43:58 +0530

    minor changes #15

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,5 +1,3 @@
-const apiConfig = require('../apiKeys');
-const request = require('postman-request');
 const chalk = require('chalk');
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast')
@@ -22,18 +20,3 @@ forecast(25.44202, 81.73326, (error, data) => {
         log('data:', data);
     }
 })
-
-//const url = 'http://api.weatherstack.com/current?access_key=' + apiConfig.weather_api_key + '&query=' + lat + ',' + lng + '&units=m';
-
-// request({ url: url, json: true }, (error, response) => {
-//     if (error) {
-//         log(chalk.red.inverse('error in connection'));
-//     }
-//     else if (response.body.error) {
-//         log(chalk.red.inverse('unable to find location'));
-//     }
-//     else {
-//         log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degrees out. There is a ' + response.body.current.precip + ' % chance of rain.');
-//     }
-
-// });
