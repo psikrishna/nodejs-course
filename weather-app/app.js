33a48b711740b0b2b8336078b605b3369c40c20b
psikrishna <attalurisaikrishna@gmail.com>
2020-06-21 20:12:03 +0530

    api added

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,2 +1,12 @@
+const apiConfig = require('../apiKeys')
+const request = require('postman-request');
 const log = console.log;
-log('dodo');
+
+//log(apiConfig.weather_api_key);
+
+const url = 'http://api.weatherstack.com/current?access_key=' + apiConfig.weather_api_key + '&query=New%20York';
+
+request({ url: url }, (error, response) => {
+    const data = JSON.parse(response.body);
+    log(data);
+});
