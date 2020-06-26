2a5ac94d8409f20dc8b7ffe183efb62f92fde9ad
psikrishna <attalurisaikrishna@gmail.com>
2020-06-27 02:47:06 +0530

    prepare js file to handle queries completed, styling and deployment left

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,13 +1,21 @@
 const log = console.log;
 
-fetch('http://localhost:3000/weather?address=@@').then((response)=>{
-    response.json().then((data)=>{
-        if(data.error){
-            log(data.error)
-        }
-        else{
-            log(data.location)
-            log(data.forecast)
-        }
-    })
+const weatherForm = document.querySelector('form');
+
+const search = document.querySelector('input');
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const location = search.value;
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                log(data.error)
+            }
+            else {
+                log(data.location);
+                log(data.forecast);
+            }
+        });
+    });
 });
